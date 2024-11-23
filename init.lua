503c63fe0d628a7d470b862a51ff20f86ad041ff
Llywelyn-Devoy <llywelyndevoy@gmail.com>
2024-11-23 10:51:45 +0000

    [ new ] pad left toggle command

--- init.lua
+++ init.lua
@@ -492,7 +492,6 @@ require('lazy').setup({
       --
       -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
       -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
@@ -967,3 +966,23 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+--
+--
+--
+--
+--
+--
+-- My changes
+require('nvim-treesitter.install').compilers = { 'zig' }
+
+vim.api.nvim_create_user_command('LDTogMid', function()
+  local on = vim.opt.foldcolumn:get() ~= '0'
+
+  if on then
+    vim.opt.foldcolumn = '0'
+  else
+    vim.opt.foldcolumn = '9'
+  end
+end, {})
+
